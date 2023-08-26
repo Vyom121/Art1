e5cc953f537da608d4e335f2cf2b4ab9259e404e
Vyom Singh <96306267+Vyom121@users.noreply.github.com>
2023-08-26 23:03:37 +0530

    Update urls.py

--- social_media/urls.py
+++ social_media/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
-    path('',include("main.urls"))
+    path('',include("main.urls")),
 ]
 
 urlpatterns= urlpatterns+static(settings.MEDIA_URL,
